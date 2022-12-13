252cf02a5787111e192f7b02966aadbe0c7ada88
binbro708 <b86070800@gmail.com>
2022-12-13 23:28:47 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,53 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "@/views/HomeView.vue";
+import About from "@/views/AboutView.vue";
+import Manage from "@/views/ManageView.vue";
+
+const routes = [
+  {
+    // 可以設置name vueRouter的to可以link name畢竟你修改網址的時候總不可能一個一個改
+    name: "home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",
+    // 別名
+    // alias: "/mange",
+    path: "/manage-music",
+    component: Manage,
+    // 只在mange防衛
+    beforeEnter: (to, from, next) => {
+      console.log("a");
+      next();
+    },
+  },
+  {
+    path: "/manage",
+    // 重新定向
+    redirect: { name: "manage" },
+  },
+  // 取所有的路由來重新定位到首頁
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-  ],
+  routes,
+  linkExactActiveClass: "text-yellow-500",
 });
+
+// 全域防護
+// router.beforeEach((to, form, next) => {
+// next();
+// });
 
 export default router;
